81f9b64b670ae924d905f5f22604969058930f24
OMKAR MORE <omkar.more22@vit.edu>
2025-02-25 04:25:34 -0500

    Initial commit - Added product list and cart functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,7 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    state.push(action.payload);
     },
     removeItem: (state, action) => {
     },
